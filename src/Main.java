0678f802c28291410a785712c2939e14c22f1821
Hawohay <hawohay@outlook.com>
2024-07-25 08:58:21 -0300

    Resolução dos exercícios 1,2 e 3 do módulo 1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-//        Pessoa pessoa = new Pessoa();
-//        pessoa.mensagem();
-//
-//        Calculadora oDobro = new Calculadora();
-//        oDobro.numero = 5;
-//        System.out.println(oDobro.retornaODobro());
+        Pessoa pessoa = new Pessoa();
+        pessoa.mensagem();
+
+        Calculadora oDobro = new Calculadora();
+        oDobro.numero = 5;
+        System.out.println(oDobro.retornaODobro());
 
         Musica trilha1 = new Musica();
         trilha1.titulo = "Princesa teste";
         trilha1.artista = "Amado Batista";
         trilha1.anoLancamento = 1991;
-        trilha1.avaliacao = 4;
-        trilha1.numAvaliacoes = 2;
+        trilha1.avaliarMusica(4);
+        trilha1.avaliarMusica(7);
+        trilha1.avaliarMusica(8);
+        trilha1.avaliarMusica(9);
+        trilha1.avaliarMusica(9);
 
         trilha1.fichaTecnica();
 
+        Musica trilha2 = new Musica();
+        trilha1.titulo = "Sozinho";
+        trilha2.artista = "Caetano Veloso";
+        trilha2.anoLancamento = 1989;
+        trilha2.avaliarMusica(8);
+        trilha2.avaliarMusica(7);
+        trilha2.avaliarMusica(8);
+        trilha2.avaliarMusica(9);
+        trilha2.avaliarMusica(9);
+
+        trilha2.fichaTecnica();
 
     }
 }
--- src/Musica.java
+++ src/Musica.java
@@ -1,20 +1,16 @@
-//Crie uma classe Musica com atributos titulo, artista, anoLancamento, avaliacao e
-//numAvaliacoes, e métodos para exibir a ficha técnica, avaliar a música e calcular a
-//média de avaliações.
-
 public class Musica {
 
     String titulo;
     String artista;
     int anoLancamento;
     double avaliacao;
-    int numAvaliacoes;
+    int numAvaliacoes = 0;
 
     public void fichaTecnica(){
         System.out.println("Título: " + titulo);
         System.out.println("Artista: " + artista);
         System.out.println("Ano de lançamento: " + anoLancamento);
-        System.out.println("Avaliação: " + avaliacao);
+        System.out.println("Avaliação: " + mediaAvaliacoes());
     }
 
     public void avaliarMusica(double nota){
@@ -26,43 +22,4 @@ public class Musica {
         return avaliacao / numAvaliacoes;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getArtista() {
-        return artista;
-    }
-
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
-
-    public int getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public void setAnoLancamento(int anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-
-    public double getAvaliacao() {
-        return avaliacao;
-    }
-
-    public void setAvaliacao(double avaliacao) {
-        this.avaliacao = avaliacao;
-    }
-
-    public int getNumAvaliacoes() {
-        return numAvaliacoes;
-    }
-
-    public void setNumAvaliacoes(int numAvaliacoes) {
-        this.numAvaliacoes = numAvaliacoes;
-    }
 }
