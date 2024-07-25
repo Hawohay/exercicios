fbc49d1b17f3093eecfc5b7e3715e1f96a1c8b63
Hawohay <hawohay@outlook.com>
2024-07-25 20:24:49 -0300

    Resolução do exercício 1 do módulo 3.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        Livro livro1 = new Livro();
-        String autor = livro1.getAutor();
-        String titulo = livro1.getTitulo();
-
-        System.out.println("Autor: " + autor + "<> " + "Título: " + titulo);
-
-        livro1.setAutor("Hawohay");
-        livro1.setTitulo("Codando em Java");
-
-        livro1.exibirDetalhes();
 
     }
 }
