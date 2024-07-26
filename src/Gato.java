ee56b1c34dfab71f8a57c7d28fb6dd120eb886af
Hawohay <hawohay@outlook.com>
2024-07-26 11:42:14 -0300

    Resolução do exercício 2 do módulo 3.

---
+++ src/Gato.java
@@ -0,0 +1,13 @@
+public class Gato extends Animal{
+
+    @Override
+    public void emitirSom() {
+        super.emitirSom();
+        System.out.println("Miauuuuuuuuu!!!!");
+    }
+
+    public void arranharMoveis(){
+        System.out.println("Arranhando um sofazinho");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Cachorro doguinho = new Cachorro();
+        doguinho.emitirSom();
+        doguinho.abanarRabo();
+
+        Gato baltazar = new Gato();
+        baltazar.emitirSom();
+        baltazar.arranharMoveis();
 
     }
 }
