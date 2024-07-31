db76e0555a9331035e9ff0a8ec32607fa0efe610
Hawohay <hawohay@outlook.com>
2024-07-31 12:00:13 -0300

    Resolução do exercício 1 do módulo 1 (Trabalhando com listas e coleções de dados).

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Livro livro1 = new Livro();
-        livro1.calcularPrecoFinal(100);
-        System.out.println(livro1.getPrecoFinal());
+       Pessoa pessoa1 = new Pessoa();
+       pessoa1.setNome("Hawohay");
+       pessoa1.setIdade(41);
 
-        ProdutoFisico produto1 = new ProdutoFisico();
-        produto1.calcularPrecoFinal(100);
-        System.out.println(produto1.getPrecoFinal());
+        System.out.println(pessoa1);
 
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,5 +1,25 @@
 public class Pessoa {
-    public void mensagem(){
-        System.out.println("Hello world!");
+    String nome;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return ("Nome: " + nome + " -" + " idade: " + idade);
     }
 }
